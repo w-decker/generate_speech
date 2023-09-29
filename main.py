19ceabb5aafdce3c8cecb52eb3d42388b5f4317a
Your Name <deckerwill7@gmail.com>
2023-09-29 10:10:35 -0500

    scramble

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ import torch
 import pandas as pd
 # from itertools import combinations
 
-# set engines
+# set engines (run every time)
 processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts")
 model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts")
 vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan")
 
-# load xvector containing speaker's voice characteristics from a dataset
+# load xvector containing speaker's voice characteristics from a dataset (run every time)
 embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
 speaker_embeddings = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0)
 
@@ -31,9 +31,9 @@ sf.write("sounds/speech.wav", speech.numpy(), samplerate=16000)
 # empty df
 d = pd.DataFrame()
 d.columns = ['s1', 's2', 's3']
-d['s1'] = ['pee', 'too', 'bee', 'boo', 'paa', 'daa']
+d['s1'] = ['dee', 'doo', 'daa', 'tee', 'too', 'taa']
 d['s2'] = ['pee', 'too', 'bee', 'boo', 'paa', 'daa']
-d['s3'] = ['pee', 'too', 'bee', 'boo', 'paa', 'daa']
+d['s3'] = ['poo', 'too', 'too', 'pee', 'daa', 'dee']
 
 # loop through df and make sounds
 for i in range(len(d)):
@@ -42,5 +42,5 @@ for i in range(len(d)):
     speech = model.generate_speech(inputs["input_ids"], speaker_embeddings, 
                                    vocoder=vocoder)
     spectrogram = model.generate_speech(inputs["input_ids"], speaker_embeddings)
-    sf.write("sounds/" + f"{d['s1'][i]}" + f"{d['s2'][i]}" + f"{d['s3'][i]}" + ".wav", 
+    sf.write("sounds/" + f"{d['s1'][i]}-" + f"{d['s2'][i]}-" + f"{d['s3'][i]}" + ".wav", 
              speech.numpy(), samplerate=16000)
