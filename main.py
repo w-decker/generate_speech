f03ef52351b68399e67876c5e1ea511814b91db7
Your Name <deckerwill7@gmail.com>
2023-09-29 20:28:00 -0500

    syllable permutation

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 
 # imports
-from transformers import SpeechT5Processor, SpeechT5ForTextToSpeech,SpeechT5HifiGan
+from transformers import SpeechT5Processor, SpeechT5ForTextToSpeech, SpeechT5HifiGan
 import soundfile as sf
 from datasets import load_dataset
 import torch
 import pandas as pd
-# from itertools import combinations
+from itertools import permutations
 
 # set engines (run every time)
 processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts")
@@ -44,3 +44,19 @@ for i in range(len(d)):
     spectrogram = model.generate_speech(inputs["input_ids"], speaker_embeddings)
     sf.write("sounds/" + f"{d['s1'][i]}-" + f"{d['s2'][i]}-" + f"{d['s3'][i]}" + ".wav", 
              speech.numpy(), samplerate=16000)
+    
+############ fullscale ############
+
+# list all possible syllables
+syl2perm = ('pee', 'poo', 'paa', 'tee', 'too', 'taa', 'bee', 'boo', 'baa', 'dee', 'doo', 'daa')
+
+# permute all combinations of three syllables
+perm = permutations(syl2perm, r=3)
+holding = []
+for i in list(perm):
+    holding.append(i)
+
+holding = list(holding)
+
+# create data frame of permuted syllables
+d = pd.DataFrame(holding, columns=['s1', 's2', 's3'])
